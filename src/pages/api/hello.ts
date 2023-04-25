f0ca94115de10a9f069e6a39079b32c64a0acee1
Luan Kisaki <luankisaki@gmail.com>
2023-04-25 16:35:13 -0300

    timestamp-new-date

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,11 +2,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
+  timestamp: Date
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Luan Kisaki' })
+  const timestamp = new Date()
+  res.status(300).json({ name: 'Luan Kisaki', timestamp })
 }
